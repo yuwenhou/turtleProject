85d445b7f4e224b6af0203c865b441c1ba11b33e
houyuwen <houyuwen2014@163.com>
2019-07-23 10:35:35 +0800

    小卓佩奇

---
+++ test.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.begin_poly()
+
+turtle.fd(100)
+turtle.left(144)
+turtle.fd(100)
+turtle.left(144)
+turtle.fd(100)
+turtle.left(144)
+turtle.fd(100)
+turtle.left(144)
+turtle.fd(100)
+turtle.left(144)
+
+turtle.end_poly()
+turtle.get_poly()
